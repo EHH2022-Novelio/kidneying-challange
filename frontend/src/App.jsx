19c8cc91e4aaff2f0ac1bbea5112e8ee7c00960e
Lev Andreev <duffyduck451@gmail.com>
2022-11-26 14:13:29 +0100

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,19 @@ import Footer from './Components/Navbar/footer';
 import Landing from './Components/LandingPage/Landing';
 import LogIn from './Components/auth/Login';
 import Sign from './Components/auth/Register';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Nav logged={false}/>
-      <Landing />
-      {/* <LogIn />
-      <Sign /> */}
+      <Routes>
+        <Route path='/' element={<Landing />}></Route>
+        <Route path='/login' element={<LogIn />}></Route>
+        <Route path='/registere' element={<Sign />}></Route>
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
